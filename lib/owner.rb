a113c05b515f1878227164c3c7ba43f4f2da027b
Ben Blackman <bblkmn5@gmail.com>
2017-10-23 22:18:14 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,8 @@
 class Owner
-  #can have a name, pets attribute as a hash with 3 keys
+#can have a name, pets attribute as a hash with 3 keys
   attr_accessor :name, :pets
-  #can't change its species
+  
+#can't change its species
   attr_reader :species
 
 #keeps track of the owners that have been created
@@ -9,20 +10,23 @@ class Owner
   def self.all
     @@owners
   end
+  
 #can reset the owners that have been created
   def self.reset_all
     @@owners.clear
   end
+  
 #can count how many owners have been created
   def self.count
     @@owners.size
   end
+  
 #initializes with a species
   def initialize(species)
     @species = species
-    #is initialized with a pets attrbute as a hash with 3 keys
+#is initialized with a pets attribute as a hash with 3 keys
     @pets = {:fishes => [], :dogs => [], :cats => []}
-    #keeps track of the owners that have been created
+  #keeps track of the owners that have been created
     @@owners << self
   end
 
