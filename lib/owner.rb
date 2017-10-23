8b99fb124ad77e1d472be18cd6ae9a172ca46e24
Ben Blackman <bblkmn5@gmail.com>
2017-10-23 22:16:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,21 +1,21 @@
 class Owner
   attr_accessor :name, :pets
   attr_reader :species
-
+  
+#keeps track of the owners that have been created
   @@owners = []
-
   def self.all
     @@owners
   end
-  
+#can reset the owners that have been created
   def self.reset_all
     @@owners.clear
   end
-
+#can count how many owners have been created
   def self.count
     @@owners.size
   end
-  
+
   def initialize(species)
     @species = species
     @pets = {:fishes => [], :dogs => [], :cats => []}
@@ -24,8 +24,8 @@ class Owner
 
 
 
-  
 
-  
+
+
 
 end
